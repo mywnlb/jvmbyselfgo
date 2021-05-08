4d8820e56b4e4ad6b46b6de1412e26d904189604
libo <623178903@qq.com>
2021-05-08 17:35:07 +0800

    读取class文件

--- src/go/ch03/classfile/class_file.go
+++ src/go/ch03/classfile/class_file.go
@@ -42,6 +42,42 @@ type ClassFile struct {
 	attributes        []*AttirbuteInfo
 }
 
+func (self *ClassFile) read(reader *ClassReader) {
+	self.readAndCheckMagic(reader)
+	self.readAndCheckVersion(reader)
+	self.constantPool = readConstantPool(reader)
+	self.accessFlags = reader.readUint16()
+	self.thisClass = reader.readUint16()
+	self.superClass = reader.readUint16()
+	self.interfaces = reader.readUint16s()
+	self.fields = readMember(reader, self.constantPool)
+	self.methods = readMember(reader, self.constantPool)
+	self.attributes = readAttributes(reader, self.constantPool)
+}
+
+func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
+	magic := reader.readUint32()
+	if magic != 0xCAFEBABE {
+		panic("java.land.ClassFormatError: magic!")
+	}
+}
+
+func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
+	self.minorVersion = reader.readUint16()
+	self.majorVersion = reader.readUint16()
+
+	switch self.majorVersion {
+	case 45:
+		return
+	case 46, 47, 48, 49, 50, 51, 52:
+		if self.minorVersion == 0 {
+			return
+		}
+	}
+
+	panic("java.lang.UnsupportedClassVersionError!")
+}
+
 func Parse(classData []byte) (cf *ClassFile, err error) {
 	defer func() {
 		if r := recover(); r != nil {
@@ -57,4 +93,23 @@ func Parse(classData []byte) (cf *ClassFile, err error) {
 	cf = &ClassFile{}
 	cf.read(cr)
 	return
+}
+
+func (self *ClassFile) MinorVersion() uint16 {
+	return self.minorVersion
+}
+func (self *ClassFile) MajorVersion() uint16 {
+	return self.majorVersion
+}
+func (self *ClassFile) ConstantPool() ConstantPool {
+	return self.constantPool
+}
+func (self *ClassFile) AccessFlags() uint16 {
+	return self.accessFlags
+}
+func (self *ClassFile) Fields() []*MemberInfo {
+	return self.fields
+}
+func (self *ClassFile) Methods() []*MemberInfo {
+	return self.methods
 }

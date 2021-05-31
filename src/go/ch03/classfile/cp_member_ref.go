51d05e769b947023403cad3a92f38bd93705e1cc
libo <623178903@qq.com>
2021-05-31 21:09:51 +0800

    读取class文件

---
+++ src/go/ch03/classfile/cp_member_ref.go
@@ -0,0 +1,19 @@
+package classfile
+
+/*
+CONSTANT_Fieldref_info {
+    u1 tag;
+    u2 class_index;
+    u2 name_and_type_index;
+}
+CONSTANT_Methodref_info {
+    u1 tag;
+    u2 class_index;
+    u2 name_and_type_index;
+}
+CONSTANT_InterfaceMethodref_info {
+    u1 tag;
+    u2 class_index;
+    u2 name_and_type_index;
+}
+*/
